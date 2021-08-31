705d3de0db76b8679bfd8134330994509445a67b
Ohminhyeok <69203354+5MinHyeok@users.noreply.github.com>
2021-08-31 17:56:30 +0900

    회원 웹 기능 - 홈 화면 추가, 등록, 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 // controller 는 스프링이 관리해야하기 때문에
@@ -34,5 +40,27 @@ public class MemberController {
     public MemberController(MemberService memberService) { // 생성자로 memvberService 를 연결
         this.memberService = memberService; // 생성자 주입(그 외에 setter 주입, 필드주입이 있음 / 생성자 주입을 가장 권장)
         // 멤버컨트롤러가 생성될때 스프링 빈에 등록되어있는 멤버 서비스 객체를 가져와서 넣어줌 -> DI(의존성 주입)
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);  // 회원가입
+
+        return "redirect:/"; // 회원가입이 끝나면 홈 화면으로 보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
     }
 }
